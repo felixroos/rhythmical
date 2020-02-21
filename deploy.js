44a706367f515da651761dbbece3725c5c88fbec
Felix Roos <roos@entrecode.de>
2020-02-21 12:01:51 +0100

    fix deploy script

--- deploy.js
+++ deploy.js
@@ -1,5 +1,8 @@
 var ghpages = require('gh-pages');
 
 ghpages.publish('dist', function(err) {
-  console.log('error', err);
+  if (err) {
+    console.log('error', err);
+  }
+  console.log('published!');
 });
